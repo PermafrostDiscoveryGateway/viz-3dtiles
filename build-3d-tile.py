0f7bd1527f1ab996b6717591ad500a5f90574407
laurenwalker <walker@nceas.ucsb.edu>
2022-03-14 22:40:09 -0400

    Fixed issue where the converted features were all lumped together as one model. Now the geometries are sent to py3dtiles as an array of geometries, and the 'batched' flag is set to true so the batchId is inserted into the b3dm to assign a batchId (i.e. model id) to each vertex.

--- build-3d-tile.py
+++ build-3d-tile.py
@@ -6,6 +6,7 @@ from py3dtiles import GlTF, TriangleSoup, B3dm
 from pprint import pprint
 import os
 import json
+import math
 
 # The file to convert to b3dm and the path to save the b3dm to
 file="/data/example.shp"
@@ -17,6 +18,9 @@ print("Converting ", filepath)
 
 # Save as - the file name of the result .b3dm file
 save_as_filename="model"
+
+# Limit the number of features converted. Useful for testing huge files.
+max_features=15
 
 tileset = {
     "asset" : {
@@ -46,18 +50,22 @@ point_x_values_4978 = [] # Used to calculate max/min for bounding box
 point_y_values_4978 = [] # Used to calculate max/min for bounding box
 point_z_values_4978 = [] # Used to calculate max/min for bounding box
 
+#gdf['geometry'] = gdf['geometry'].simplify(tolerance)
+
 row = 0
 for feature in gdf.iterfeatures():
+    if row > max_features-1:
+        break
     polygon = Polygon(feature["geometry"]["coordinates"][0])
     
     # Replace the row's geometry in the GeoDataFrame
     print(f"Processing {row + 1} of {len(gdf)}")
     ext_ring_z = []
 
-    for point in polygon.exterior.coords:
+    for point in polygon.convex_hull.exterior.coords:
         # create an exterior ring with xyz values, not just xy
         # Use a .1 meter elevation for now. May need to dynamically retrieve this from the data if elevation is there
-        x,y,z = point[0], point[1], 0.1
+        x,y,z = point[0], point[1], 50
         ext_ring_z.append((x, y, z))
     # Build the new PolygonZ
     polygon_z = Polygon(ext_ring_z)
@@ -70,13 +78,20 @@ for feature in gdf.iterfeatures():
 gdf_4978 = gdf.to_crs(epsg=4978)
 
 polygons_z_4978 = [] # List of polygons for glTF Multipolygon
+geometries=[]
 row = 0
 for feature in gdf_4978.iterfeatures():
+    if row > max_features-1:
+        break
     print(f"Processing {row + 1} of {len(gdf_4978)} in EPSG", final_epsg)
-
+    
     #Create a PolygonZ
     polygon_4978 = Polygon(feature["geometry"]["coordinates"][0])
-        
+
+    point_x_this_polygon=[]
+    point_y_this_polygon=[]
+    point_z_this_polygon=[]
+
     # Cache the x,y,z values for max/min calc later
     # TODO: Look into faster ways to do this
     for point in polygon_4978.exterior.coords:
@@ -86,28 +101,34 @@ for feature in gdf_4978.iterfeatures():
         point_x_values_4978.append(x)
         point_y_values_4978.append(y)
         point_z_values_4978.append(z)
-    # Save the Polygon to a list
-    polygons_z_4978.append(polygon_4978)
+
+        point_x_this_polygon.append(x)
+        point_y_this_polygon.append(y)
+        point_z_this_polygon.append(z)
+
+    # Use the simplified polygon to create a WKB
+    miltipolygon_z_4978 = MultiPolygon([polygon_4978])
+
+    # use the TriangleSoup helper class to transform the wkb into arrays
+    # of points and normals
+    print(f"Tesselating polygon to generate position and normal arrays")
+    ts = TriangleSoup.from_wkb_multipolygon(miltipolygon_z_4978.wkb)
+    positions = ts.getPositionArray()
+    normals = ts.getNormalArray()
+
+    box_mins = [min(point_x_this_polygon), min(point_y_this_polygon), min(point_z_this_polygon)]
+    box_maxs = [max(point_x_this_polygon), max(point_y_this_polygon), max(point_z_this_polygon)]
+    box_degrees = [box_maxs, box_mins]
+
+    # generate the glTF part from the binary arrays.
+    geometries.append({ 'position': positions, 'normal': normals, 'bbox': box_degrees})
+
     row += 1
 
-# Use the simplified polygon to create a WKB
-miltipolygon_z_4978 = MultiPolygon(polygons_z_4978)
-
-
-# --- Convert to gltf -----
-# use the TriangleSoup helper class to transform the wkb into arrays
-# of points and normals
-print(f"Tesselating polygons to generate position and normal arrays")
-ts = TriangleSoup.from_wkb_multipolygon(miltipolygon_z_4978.wkb)
-positions = ts.getPositionArray()
-normals = ts.getNormalArray()
-
 print(f"Creating bounding box for gltf - EPSG ", final_epsg)
-import math
 box_mins_4978 = [min(point_x_values_4978), min(point_y_values_4978), min(point_z_values_4978)]
 box_maxs_4978 = [max(point_x_values_4978), max(point_y_values_4978), max(point_z_values_4978)]
 box_degrees_4978 = [box_maxs_4978, box_mins_4978]
-
 
 transform = np.array(
     [[
@@ -119,10 +140,8 @@ transform = np.array(
 
 transform = transform.flatten('F')
 
-# generate the glTF part from the binary arrays.
-geometry = { 'position': positions, 'normal': normals, 'bbox': box_degrees_4978}
 print("creating glTF")
-gltf = GlTF.from_binary_arrays([geometry], transform=transform, batched=False)
+gltf = GlTF.from_binary_arrays(geometries, transform=transform, batched=True)
 
 #with open(save_as+".glb", "bw") as f:
 #   f.write(bytes(gltf.to_array()))
