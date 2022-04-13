9d3d5d4a03238912f4588448e974bdfb94e0344e
laurenwalker <walker@nceas.ucsb.edu>
2022-04-13 16:28:47 -0400

    Change build-output dir to test

--- test/run-cesium/cesium.js
+++ test/run-cesium/cesium.js
@@ -18,10 +18,10 @@ scene.globe.depthTestAgainstTerrain = true;
 
 // The tileset created by the build-3d-tile.py script
 var tileset = new Cesium.Cesium3DTileset({
-  url: "tilesets/build-output/tileset.json",
-  debugShowBoundingVolume: true,
-  debugShowContentBoundingVolume: true,
-  debugShowGeometricError: true,
+  url: "tilesets/test/tileset.json",
+  debugShowBoundingVolume: false,
+  debugShowContentBoundingVolume: false,
+  debugShowGeometricError: false,
   debugWireframe: false
 });
 
@@ -61,65 +61,6 @@ window.zoom_to_me = function(){
 }
 
 tileset.readyPromise.then(zoom_to_me).otherwise(error => { console.log(error) });
-
-// Below code is experiemental and not executed currently
-var center = new Cesium.Cartesian3.fromDegrees(-762889.979147543, -1335791.868934655, 6169085.401505229);
-var posMat = Cesium.Transforms.eastNorthUpToFixedFrame(center);
-
-function updateTile (tile) {
-    var boundingVolume = tile.boundingVolume;
-    if (Cesium.defined(tile.contentBoundingVolume)) {
-        boundingVolume = tile.contentBoundingVolume;
-    }
-    var content = tile.content;
-    var model = content._model;
-    var height = boundingVolume.minimumHeight || 0;
-    var center = model._rtcCenter || new Cesium.Cartesian3(-762889.9791526495,
-                -1335791.8689435967,
-                6169085.401505229);
-    var normal = scene.globe.ellipsoid.geodeticSurfaceNormal(center, new Cartesian3());
-    var offset = Cartesian3.multiplyByScalar(normal, height, new Cartesian3());
-    var carto = Cesium.Cartographic.fromCartesian(center);
-    var promise = Cesium.when.defer();
-    if (scene.terrainProvider === ellipsoidTerrainProvider) {
-        var result = carto;
-        result.height = 0;
-        promise.resolve(result);
-    } else {
-        promise = Cesium.sampleTerrainMostDetailed(scene.terrainProvider, [carto]).then(function(results) {
-            var result = results[0];
-            if (!Cesium.defined(result)) {
-                return carto;
-            }
-            return result;
-        });
-    }
-
-    promise.then(function(result) {
-        result = Cesium.Cartographic.toCartesian(result);
-        var position = Cartesian3.subtract(result, offset, new Cartesian3());
-        model._rtcCenter = Cartesian3.clone(position, model._rtcCenter);
-    });
-}
-
-function updateTileset(root) {
-    if (root.contentReady) {
-        updateTile(root);
-    } else {
-        var listener = tileset.tileLoad.addEventListener(function(tile) {
-            if (tile === root) {
-                updateTile(tile);
-                listener();
-            }
-        });
-    }
-
-    var children = root.children;
-    var length = children.length;
-    for (var i = 0; i < length; ++i) {
-        updateTileset(children[i]);
-    }
-}
 
 }
 
--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ import os
 # usage: from ./viz-3dtiles run `python test/test.py`
 
 # Output Tileset directory -- view `tileset.json`
-save_to=os.path.dirname(os.path.abspath(__file__))+"/run-cesium/tilesets/build-output/"
+save_to=os.path.dirname(os.path.abspath(__file__))+"/run-cesium/tilesets/test/"
 
 # Create a 3D Tile from the Example shp file
 tile = Cesium3DTile()
