55b270b96bdb00fed653c36cb3696bf858d64622
Robyn Thiessen-Bock <robyn.thiessenbock@gmail.com>
2022-06-07 16:59:54 -0400

    Add get_min_bounding_rect method
    Relates to issue #19

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name='viz_3dtiles',
         'numpy >= 1.20, < 2.0',
         'pandas >= 1.4, < 2.0',
         'geopandas >= 0.10, < 1.0',
-        'pdgpy3dtiles @ git+https://github.com/PermafrostDiscoveryGateway/py3dtiles.git#egg=pdgpy3dtiles'
+        'pdgpy3dtiles @ git+https://github.com/PermafrostDiscoveryGateway/py3dtiles.git#egg=pdgpy3dtiles',
+        'scipy >= 1.8, < 2.0',
       ],
       zip_safe=False)
--- viz_3dtiles/Cesium3DTileset.py
+++ viz_3dtiles/Cesium3DTileset.py
@@ -3,6 +3,8 @@ import json
 import os
 from pathlib import Path
 from statistics import mean
+import numpy as np
+from scipy.spatial import ConvexHull
 
 
 class Cesium3DTileset:
@@ -60,6 +62,79 @@ class Cesium3DTileset:
         self.bbox = bounding_volume_box
 
         return bounding_volume_box
+
+    def get_min_bounding_rect(gdf):
+        """
+            Find the smallest bounding rectangle for a set of polygons in a GeoDataFrame.
+            Returns a set of points representing the corners of the bounding box.
+            Adapted from https://stackoverflow.com/a/33619018
+
+            Parameters
+            ----------
+
+            gdf : GeoDataFrame
+                A GeoDataFrame containing polygons.
+
+            Returns
+            -------
+            A set of points representing the corners of the bounding box.
+        """
+
+        # Get all points from all polygons, as a nx2 matrix of coordinates
+        coords = gdf.geometry.apply(lambda x: x.exterior.coords)
+        points = np.vstack([p for p in coords])
+
+        pi2 = np.pi / 2.
+
+        # get the convex hull for the points
+        hull_points = points[ConvexHull(points).vertices]
+
+        # calculate edge angles
+        edges = np.zeros((len(hull_points) - 1, 2))
+        edges = hull_points[1:] - hull_points[:-1]
+
+        angles = np.zeros((len(edges)))
+        angles = np.arctan2(edges[:, 1], edges[:, 0])
+
+        angles = np.abs(np.mod(angles, pi2))
+        angles = np.unique(angles)
+
+        # find rotation matrices
+        rotations = np.vstack([
+            np.cos(angles),
+            np.cos(angles - pi2),
+            np.cos(angles + pi2),
+            np.cos(angles)]).T
+
+        rotations = rotations.reshape((-1, 2, 2))
+
+        # apply rotations to the hull
+        rot_points = np.dot(rotations, hull_points.T)
+
+        # find the bounding points
+        min_x = np.nanmin(rot_points[:, 0], axis=1)
+        max_x = np.nanmax(rot_points[:, 0], axis=1)
+        min_y = np.nanmin(rot_points[:, 1], axis=1)
+        max_y = np.nanmax(rot_points[:, 1], axis=1)
+
+        # find the box with the best area
+        areas = (max_x - min_x) * (max_y - min_y)
+        best_idx = np.argmin(areas)
+
+        # return the best box
+        x1 = max_x[best_idx]
+        x2 = min_x[best_idx]
+        y1 = max_y[best_idx]
+        y2 = min_y[best_idx]
+        r = rotations[best_idx]
+
+        rval = np.zeros((4, 2))
+        rval[0] = np.dot([x1, y2], r)
+        rval[1] = np.dot([x2, y2], r)
+        rval[2] = np.dot([x2, y1], r)
+        rval[3] = np.dot([x1, y1], r)
+
+        return rval
 
     def to_json(self):
 
