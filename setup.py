7104da111a6f6ffa956db78bd8283c255712b7b6
Alyona <alonakosobokova@gmail.com>
2025-10-06 12:50:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name="viz_3dtiles",
-    version="0.0.1",
+    version="1.0.0",
     description="A module that converts vector geometry files to Cesium 3D Tilesets",
     url="https://github.com/PermafrostDiscoveryGateway/viz-3dtiles",
-    author="Lauren Walker",
-    author_email="walker@nceas.ucsb.edu",
+    author=(
+        "Robyn Thiessen-Bock; Juliet Cohen; Matthew B. Jones; "
+        "Kastan Day; Lauren Walker; Rushiraj Nenuji; Alyona Kosobokova"
+    ),
     license="Apache License Version 2.0",
     packages=["viz_3dtiles"],
     python_requires=">=3.5",
